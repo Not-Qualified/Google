4469adb762a15c5bde18d065033e0976488fbf0f
Penguina <dharmesh.srk@gmail.com>
2021-01-12 17:27:11 +0530

    Update

--- get_list.py
+++ get_list.py
@@ -10,25 +10,24 @@ import csv
 driver = webdriver.Chrome("chromedriver")
 # driver.get("http://google.com/search?q=selena+gomez")
 # driver.get("https://google.com/search?q=Best+Hollywood+Celebrities")
-count = 0
+# count = 0
 
 file = open("list.txt", "r")
 lines = file.readlines()
 for line in lines:
-	print(line)
 	driver.get(f"http://google.com/search?q={line}")
 	name = driver.find_element_by_css_selector("input[name='q']").get_attribute("value")
 	print(name)
 	# Getting Data
 	# global count
 	# count += 1
-	time.sleep(random.randint(0, 2))
+	time.sleep(random.randint(0, 3))
 	data = driver.find_elements_by_css_selector(".mod g-link a")
 	music_data = driver.find_elements_by_css_selector("div[class='scrt'] a")
 	social_links = []
 	social_links.append(name)
 
-	with open("first.csv", "a", newline="") as cf:
+	with open("database.csv", "a", newline="") as cf:
 		writer = csv.writer(cf, delimiter="|", )
 		for music in music_data:
 			social_links.append(music.get_attribute("text"))
@@ -40,4 +39,3 @@ for line in lines:
 			social_links.append(link.get_attribute("href"))
 			print(link.get_attribute("text"), link.get_attribute("href"))
 		writer.writerow(social_links)
-
